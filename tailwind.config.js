8127d8d1561ae8af44a847b6ca89e1e8cf555ff9
adamhs29 <adam.harnando.s@gmail.com>
2023-12-18 17:23:08 +0700

    portfolio tailwind css

---
+++ tailwind.config.js
@@ -0,0 +1,22 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["index.html"],
+  darkMode: "class",
+  theme: {
+    container: {
+      center: true,
+      padding: "16px",
+    },
+    extend: {
+      colors: {
+        primary: "#38bdf8",
+        secondary: "#9ca3af",
+        dark: "#0f172a",
+      },
+      screens: {
+        "2xl": "1320px",
+      },
+    },
+  },
+  plugins: [],
+};
